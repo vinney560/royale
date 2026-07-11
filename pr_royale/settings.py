ecb0462b282fb6a50d0a56a924eaa972a93c05b5
vinney560 <vinneyjoy1@gmail.com>
2026-07-11 18:44:21 +0300

    Signed-off-by: vinney560 <vinneyjoy1@gmail.com>

--- pr_royale/settings.py
+++ pr_royale/settings.py
@@ -3,22 +3,13 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool, default=False)
 
 ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +28,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Whitenoise middleware
+    # https://warehouse.python.org/project/whitenoise/
+    # https://warehouse.python.org/project/whitenoise/django/
+    # https://warehouse.python.org/project/whitenoise/django/whitenoise/
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
